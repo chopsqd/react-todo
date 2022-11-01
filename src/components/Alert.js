f02e56681c7b3be76d8d1dab64c283fa52ca4541
Michael Chop <007micle007@gmail.com>
2022-11-01 22:14:57 +0300

    Added some animations for notes, using react-transition-group package

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,18 +1,27 @@
 import React, {useContext} from 'react'
+import {CSSTransition} from 'react-transition-group'
 import {AlertContext} from "../context/alert/alert-context";
 
 export const Alert = () => {
     const {alert, hide} = useContext(AlertContext)
 
-    if(!alert.visible) {
-        return null
-    }
     return (
-        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
-            <strong>Внимание!</strong>
-            &nbsp;{alert.text}
-            <button onClick={hide} type="button" className="btn-close" aria-label="Close"/>
-        </div>
+        <CSSTransition
+            in={alert.visible}
+            timeout={{
+                enter: 500,
+                exit: 350
+            }}
+            classNames={'alert'}
+            mountOnEnter
+            unmountOnExit
+        >
+            <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
+                <strong>Внимание!</strong>
+                &nbsp;{alert.text}
+                <button onClick={hide} type="button" className="btn-close" aria-label="Close"/>
+            </div>
+        </CSSTransition>
     )
 }
 
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,34 +1,38 @@
 import React, {useContext} from 'react'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 import {AlertContext} from "../context/alert/alert-context";
 
 export const Notes = ({notes, onRemove}) => {
     const alert = useContext(AlertContext)
 
     return (
-        <ul className="list-group">
+        <TransitionGroup component={"ul"} className="list-group">
             {
                 notes.map(note => (
-                    <li
-                        className="list-group-item note"
+                    <CSSTransition
                         key={note.id}
+                        classNames={"note"}
+                        timeout={800}
                     >
-                        <div>
-                            <b>{note.title}</b>
-                            <small>{note.date}</small>
-                        </div>
-                        <button
-                            type="button"
-                            className="btn btn-outline-danger btn-sm"
-                            onClick={async () => {
-                                await onRemove(note.id)
-                                alert.show('Заметка была удалена', 'warning')
-                            }}
-                        >
-                            &times;
-                        </button>
-                    </li>
+                        <li className="list-group-item note">
+                            <div>
+                                <b>{note.title}</b>
+                                <small>{note.date}</small>
+                            </div>
+                            <button
+                                type="button"
+                                className="btn btn-outline-danger btn-sm"
+                                onClick={async () => {
+                                    await onRemove(note.id)
+                                    alert.show('Заметка была удалена', 'warning')
+                                }}
+                            >
+                                &times;
+                            </button>
+                        </li>
+                    </CSSTransition>
                 ))
             }
-        </ul>
+        </TransitionGroup>
     )
 }
